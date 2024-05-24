56225ddb93de73c4f0ed287a6e4071ec15c8b282
rawsun007 <roshanramani.dev@gmail.com>
2024-05-24 12:04:57 +0530

    init

---
+++ recipe/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class fastfood(models.Model):
+    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
+    name=models.TextField(max_length=100)
+    about=models.TextField(max_length=200)
+    img=models.ImageField(upload_to="media")
